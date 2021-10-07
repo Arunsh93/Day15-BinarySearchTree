0870bcfde61df8d4eb54c71dd67946053c91377e
Arunsh93 <Arunhosur93@gmail.com>
2021-10-07 10:50:16 +0530

    Binary search tree example in class

---
+++ BinaryTreeExample/BinarySearchTree.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTreeExample
+{
+    class BinarySearchTree
+    {
+        public Node Root { get; set; }
+
+        public bool Add(int value)
+        {
+            Node before = null, after = this.Root;
+            while(after != null)
+            {
+                before = after;
+                if (value < after.Data)
+                {
+                    after = after.LeftNode;
+                }
+                else if (value > after.Data)
+                {
+                    after = after.RightNode;
+                }
+                return false;
+                
+            }
+            Node newNode = new Node();
+            newNode.Data = value;
+            if(this.Root == null)
+            {
+                this.Root = newNode;
+            }
+            else
+            {
+                if(value<before.Data)
+                {
+                    before.LeftNode = newNode;
+                }
+                else
+                {
+                    before.RightNode = newNode;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ BinaryTreeExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BinaryTreeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree Program");
+            BinarySearchTree binaryTree = new BinarySearchTree();
+            binaryTree.Add(1);
+            binaryTree.Add(2);
+            binaryTree.Add(7);
+            binaryTree.Add(3);
+
+        }
+    }
+}
